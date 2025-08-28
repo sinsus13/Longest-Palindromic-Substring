301683bd2d5885c67cbd34fe93a9977ae19b04ab
sinsus13 <63235691+sinsus13@users.noreply.github.com>
2025-08-28 16:19:51 +0330

    Add longest palindrome finder and requirements
    Added a C++ implementation to find the longest palindromic substring in a given string. Also added a requirements.txt file specifying matplotlib for comparison plots.

---
+++ src/Dynamic programing.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(const string& s, int start, int end) {
+    while (start < end)
+    {
+        if (s[start] != s[end]) return false;
+        start++;
+        end--;
+    }
+    return true;
+}
+string longestPalindrome(string s)
+{
+    int n = s.length();
+    for (int len = n; len >= 1; len--)
+    {
+        for (int start = 0; start + len - 1 < n; start++)
+        {
+            int end = start + len - 1;
+            if (isPalindrome(s, start, end))
+            {
+                return s.substr(start, len);
+            }
+        }
+    }
+    return "";
+}
+int main()
+{
+    string input;
+	cout << "Enter a string: ";
+	cin >> input;
+	cout << "The longest palindromic substring is: " << longestPalindrome(input) << endl;
+}
