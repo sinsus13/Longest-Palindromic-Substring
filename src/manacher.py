c3ba1c8211127a16359bfd7dc263167aebbde611
Raya F <rayafaezinia@gmail.com>
2025-09-07 23:12:31 +0330

    Update manacher.py

--- src/manacher.py
+++ src/manacher.py
@@ -1,46 +1,31 @@
-#%%
 class Manacher:
-    def __init__(self, inp_str: str):
-        self.inp_str = inp_str
 
-    def process(self):
+    def process(self,inp_str: str) -> str:
+        self.inp_str = inp_str
         lst_str = ['@', '#']
         for l in self.inp_str:
             lst_str.append(l)
             lst_str.append('#')
         lst_str.append('$')
-        self.s = [0] * len(lst_str)
-        self.lst_str = lst_str
-        return self.find_manacher()
+        s = [0] * len(lst_str)
+        return self.find_manacher(s,lst_str)
 
-    def find_manacher(self):
-        n = len(self.s)
-        l = r = 0
+    def find_manacher(self,p,lst_str) :
+        n = len(p)
+        center = r = 0
         for i in range(1, n - 1):
-            flip = l + r - i  #$
-            # o1 = self.check(l, r)
-            # if o1:
-            #     return o1
+            flip = 2*center - i  #$
             if i < r:
-                self.s[i] = min(r - i, self.s[flip])
+                p[i] = min(r - i, p[flip])
             #start from idx 2
-            while self.lst_str[i + 1 + self.s[i]] == self.lst_str[i - 1 - self.s[i]]:
-                self.s[i] += 1
-            if i + self.s[i] > r:
+            while lst_str[i + 1 + p[i]] == lst_str[i - 1 - p[i]]:
+                p[i] += 1
+            if i + p[i] > r:
                 #reverse when over bound
-                l = i - self.s[i]
-                r = i + self.s[i]
-        max_len,center=max((val,idx) for idx,val in enumerate(self.s))
+                center = i 
+                r = i + p[i]
+        max_len,center=max((val,idx) for idx,val in enumerate(p))
         start=(center-max_len)//2 #2*c-i to original
         return self.inp_str[start:start+max_len]
-    #2*c-i
-    #traversing whole string even=s[cen] odd=s[cen+1]
-    # def check(self, l, r):
-    #     n = r - l + 1
-    #     c = (l + r) // 2
-    #     if n % 2 == 0:
-    #         odd = 0
-    #     else:
-    #         odd = 1
-    #     j = 2 * c + 2 + odd
-    #     return n<= j
+
+
